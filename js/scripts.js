aa9169630f9ce3f978ebb06c053582c107ad9a39
Quinten Clause <quinten.clause@studioemma.eu>
2016-05-28 21:39:37 +0200

    Test some stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
-    $.get('http://www.marktplaats.nl/z/hobby-en-vrije-tijd/verzamelkaartspellen-magic-the-gathering/magic-the-gathering.html?query=magic%20the%20gathering&categoryId=919&sortBy=standaard&sortOrder=decreasing', function (html) {
-        console.log(html);
-    }, 'html');
+    //$.get('http://www.marktplaats.nl/z/hobby-en-vrije-tijd/verzamelkaartspellen-magic-the-gathering/magic-the-gathering.html?query=magic%20the%20gathering&categoryId=919&sortBy=standaard&sortOrder=decreasing', function (html) {
+        //console.log(html);
+    //}, 'xml:w
+    //');
+    $('#container').load('http://www.marktplaats.nl/z/hobby-en-vrije-tijd/verzamelkaartspellen-magic-the-gathering/magic-the-gathering.html?query=magic%20the%20gathering&categoryId=919&sortBy=standaard&sortOrder=decreasing');
 });
